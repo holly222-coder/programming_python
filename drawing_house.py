571c9608f58fbb8a75652dd524e806c44b6550d6
holly222-coder <80085620+holly222-coder@users.noreply.github.com>
2022-02-23 16:01:14 +1100

    Add files via upload

---
+++ drawing_house.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 14:53:11 2022
+
+@author: 26902
+"""
+
+import turtle
+wn = turtle.Screen()
+bob = turtle.Turtle()
+bob.right(90)
+bob.forward(50)
+bob.left(90)
+bob.forward(50)
+
+# Add your code below!
+bob.left(90)
+bob.forward(50)
+bob.right(90)
+bob.forward(50)
+bob.left(120)
+bob.forward(130)
+bob.right(245)
+bob.forward(140)
+bob.left(130)
+bob.forward(50)
+wn.exitonclick()
