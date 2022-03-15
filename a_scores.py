655f7f0a7130a5474fc4c7b128f2c591d0ea7c74
holly222-coder <80085620+holly222-coder@users.noreply.github.com>
2022-03-15 14:53:07 +1100

    Update a_scores.py

--- a_scores.py
+++ a_scores.py
@@ -1,3 +1,5 @@
+#Below are a set of scores that students have received in the past semester. Write code to determine how many are 90 or above and assign that result to the value a_scores.
+
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 15 14:41:12 2022
