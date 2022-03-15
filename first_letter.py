3665951904c7d4cb38b8a2a8066aa396b8d39de6
holly222-coder <80085620+holly222-coder@users.noreply.github.com>
2022-03-15 15:05:29 +1100

    Update first_letter.py

--- first_letter.py
+++ first_letter.py
@@ -1,3 +1,4 @@
+#Write code that uses the string stored in org and creates an acronym which is assigned to the variable acro. Only the first letter of each word should be used, each letter in the acronym should be a capital letter, and there should be nothing to separate the letters of the acronym. Words that should not be included in the acronym are stored in the list stopwords. For example, if org was assigned the string “hello to world” then the resulting acronym should be “HW”.
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 15 15:03:38 2022
